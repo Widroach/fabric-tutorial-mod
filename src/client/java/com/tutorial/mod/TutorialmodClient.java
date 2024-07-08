3d5d49560dda9f2e1418129dbd5db3842aca8180
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-07-08 14:58:23 +0200

    Initial template file structure

---
+++ src/client/java/com/tutorial/mod/TutorialmodClient.java
@@ -0,0 +1,10 @@
+package com.tutorial.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
