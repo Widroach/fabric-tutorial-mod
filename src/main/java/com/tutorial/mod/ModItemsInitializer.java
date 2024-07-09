6fe134ca9a821e0efa77ecbc62c6385e84bbd296
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-07-09 19:42:33 +0200

    calling the init method from the ModItemsInitializer

---
+++ src/main/java/com/tutorial/mod/ModItemsInitializer.java
@@ -0,0 +1,13 @@
+package com.tutorial.mod;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class ModItemsInitializer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        SuspiciousDust.initialize();
+    }
+
+}
