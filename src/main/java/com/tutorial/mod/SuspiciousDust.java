1833beadafa9fd151224d7bf4db7167c8347d195
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-07-09 19:48:54 +0200

    adding the SuspiciousDust to the INGREDIENTS item type

--- src/main/java/com/tutorial/mod/SuspiciousDust.java
+++ src/main/java/com/tutorial/mod/SuspiciousDust.java
@@ -1,16 +1,20 @@
 package com.tutorial.mod;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 
 public class SuspiciousDust {
-    static final FoodComponent FOOD_COMPONENT = new FoodComponent(1, 1, true, 1, null, null);
+    private static final FoodComponent FOOD_COMPONENT = new FoodComponent(1, 1, true, 1, null, null);
 
     public static final Item SUSPICIOUS_DUST = ModItems.register(
             new Item(new Item.Settings().food(FOOD_COMPONENT)),
             "suspicious_dust");
 
     public static void initialize(){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+            .register((itemGroup) -> itemGroup.add(SuspiciousDust.SUSPICIOUS_DUST));
     }
 
 }
