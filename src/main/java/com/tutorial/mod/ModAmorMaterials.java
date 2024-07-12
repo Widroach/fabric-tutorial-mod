3e810977674148cde166a310eb6b481d1c4b3a1b
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-07-12 16:53:17 +0200

    register abstraction for registering armors

--- src/main/java/com/tutorial/mod/ModAmorMaterials.java
+++ src/main/java/com/tutorial/mod/ModAmorMaterials.java
@@ -1,6 +1,34 @@
 package com.tutorial.mod;
 
+import java.util.*;
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Supplier;
+
 public class ModAmorMaterials {
+
+    public static RegistryEntry<ArmorMaterial> registerMaterial(String id, Map<ArmorItem.Type, Integer> defensePoints,
+            int enchantability, RegistryEntry<SoundEvent> equipSound, Supplier<Ingredient> repairIngredientSupplier,
+            float toughness, float knockbackResistance, boolean dyeable) {
+        List<ArmorMaterial.Layer> layers = List.of(
+                new ArmorMaterial.Layer(Identifier.of("tutorial-mod", id), "", dyeable));
+
+        ArmorMaterial material = new ArmorMaterial(defensePoints, enchantability, equipSound, repairIngredientSupplier,
+                layers, toughness, knockbackResistance);
+        material = Registry.register(Registries.ARMOR_MATERIAL, Identifier.of("tutorial-mod", id),
+                material);
+
+        return RegistryEntry.of(material);
+    }
+
     public static void initialize() {
         throw new UnsupportedOperationException("Method not implemented");
     }
