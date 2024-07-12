fa8daf4f688c79bacfa2d0a62698fd1764e9e5a4
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-07-12 17:17:35 +0200

    adding the viking armor to the combat menu

--- src/main/java/com/tutorial/mod/VikingArmor.java
+++ src/main/java/com/tutorial/mod/VikingArmor.java
@@ -1,8 +1,10 @@
 package com.tutorial.mod;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.entry.RegistryEntry;
@@ -24,8 +26,22 @@ public class VikingArmor {
             0.0F,
             false);
 
-    public static final Item VIKING_HELMET = ModItems.register(new ArmorItem(VIKING_ARMOR, ArmorItem.Type.HELMET, new Item.Settings()), "viking_helmet");
-    public static final Item VIKING_VEST = ModItems.register(new ArmorItem(VIKING_ARMOR, ArmorItem.Type.CHESTPLATE, new Item.Settings()), "viking_vest");
-    public static final Item VIKING_LEGGINGS = ModItems.register(new ArmorItem(VIKING_ARMOR, ArmorItem.Type.LEGGINGS, new Item.Settings()), "viking_leggings");
-    public static final Item VIKING_BOOTS = ModItems.register(new ArmorItem(VIKING_ARMOR, ArmorItem.Type.BOOTS, new Item.Settings()), "viking_boots");
+    public static final Item VIKING_HELMET = ModItems
+            .register(new ArmorItem(VIKING_ARMOR, ArmorItem.Type.HELMET, new Item.Settings()), "viking_helmet");
+    public static final Item VIKING_VEST = ModItems
+            .register(new ArmorItem(VIKING_ARMOR, ArmorItem.Type.CHESTPLATE, new Item.Settings()), "viking_vest");
+    public static final Item VIKING_LEGGINGS = ModItems
+            .register(new ArmorItem(VIKING_ARMOR, ArmorItem.Type.LEGGINGS, new Item.Settings()), "viking_leggings");
+    public static final Item VIKING_BOOTS = ModItems
+            .register(new ArmorItem(VIKING_ARMOR, ArmorItem.Type.BOOTS, new Item.Settings()), "viking_boots");
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register(itemGroup -> {
+                    itemGroup.add(VIKING_HELMET);
+                    itemGroup.add(VIKING_VEST);
+                    itemGroup.add(VIKING_LEGGINGS);
+                    itemGroup.add(VIKING_BOOTS);
+                });
+    }
 }
