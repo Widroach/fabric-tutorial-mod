cdf5c731e973a6660b310c90f811e2a6a994f450
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-07-12 14:55:25 +0200

    Creating AncientSteelMaterial

---
+++ src/main/java/com/tutorial/mod/AncientSteelMaterial.java
@@ -0,0 +1,42 @@
+package com.tutorial.mod;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
+
+public class AncientSteelMaterial implements ToolMaterial {
+
+    @Override
+    public float getAttackDamage() {
+        return 5.0f;
+    }
+
+    @Override
+    public int getDurability() {
+        return 455;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 22;
+    }
+
+    @Override
+    public TagKey<Block> getInverseTag() {
+        return BlockTags.INCORRECT_FOR_STONE_TOOL;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 1.0f;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(SuspiciousDust.SUSPICIOUS_DUST, Items.IRON_INGOT);
+    }
+
+}
