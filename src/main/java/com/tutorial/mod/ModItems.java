3c60474d814f5e131b16defc428df4229adcfa27
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-07-13 18:25:26 +0200

    storing the modItems in a set

--- src/main/java/com/tutorial/mod/ModItems.java
+++ src/main/java/com/tutorial/mod/ModItems.java
@@ -5,10 +5,22 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+import java.util.*;
+
 public class ModItems {
+    private final Set<Item> items = new HashSet<>();
+
     public static Item register(Item item, String id) {
         Identifier itemID = Identifier.of("tutorial-mod", id);
 
         return Registry.register(Registries.ITEM, itemID, item);
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public Set<Item> getItems() {
+        return items;
     }
 }
