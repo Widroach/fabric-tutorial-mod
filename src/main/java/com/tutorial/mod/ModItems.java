59bf2e906afd9ac7bedc873b34faf1b2c52c1a51
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-07-15 18:25:02 +0200

    refactoring the ModItems as a static class to store all the items as public static fields

--- src/main/java/com/tutorial/mod/ModItems.java
+++ src/main/java/com/tutorial/mod/ModItems.java
@@ -4,14 +4,23 @@ import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.item.ArmorItem;
+
 
 public class ModItems {
 
-    public void addItem(Item item, String id) {
-        register(item, id);
+    public static final TheMightyStick THE_MIGHTY_STICK = new TheMightyStick(new Item.Settings());
+    public static final AncientSteelSword ANCIENT_STEEL_SWORD = new AncientSteelSword(new AncientSteelMaterial(), new Item.Settings());
+    public static final SuspiciousDust SUSPICIOUS_DUST = new SuspiciousDust(new Item.Settings().food(new SuspiciousDustFoodComponent().getFoodComponent()));
+    public static final VikingHelmet VIKING_HELMET = new VikingHelmet(new VikingAmorMaterial().getMaterial(), ArmorItem.Type.HELMET, new Item.Settings());
+    public static final VikingVest VIKING_VEST = new VikingVest(new VikingAmorMaterial().getMaterial(), ArmorItem.Type.CHESTPLATE, new Item.Settings());
+    public static final VikingLeggins VIKING_LEGGINS = new VikingLeggins(new VikingAmorMaterial().getMaterial(), ArmorItem.Type.LEGGINGS, new Item.Settings());
+    public static final VikingBoots VIKING_BOOTS = new VikingBoots(new VikingAmorMaterial().getMaterial(), ArmorItem.Type.BOOTS, new Item.Settings());
+
+    private ModItems() {
     }
 
-    private Item register(Item item, String id) {
+    public static Item register(Item item, String id) {
         Identifier itemID = Identifier.of("tutorial-mod", id);
 
         return Registry.register(Registries.ITEM, itemID, item);
