33dd1ee9a434a9515a7c4bd0b1aa3f5c1bdb40c5
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-07-17 21:34:55 +0200

    initializing the new potion in the game registry

--- src/main/java/com/tutorial/mod/ModItemsInitializer.java
+++ src/main/java/com/tutorial/mod/ModItemsInitializer.java
@@ -6,13 +6,15 @@ public class ModItemsInitializer implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ModItems.register(ModItems.SUSPICIOUS_DUST, "suspicious_dust");
-        ModItems.register(ModItems.THE_MIGHTY_STICK, "the_mighty_stick");
-        ModItems.register(ModItems.ANCIENT_STEEL_SWORD, "ancient_steel_sword");
-        ModItems.register(ModItems.VIKING_HELMET, "viking_helmet");
-        ModItems.register(ModItems.VIKING_VEST, "viking_vest");
-        ModItems.register(ModItems.VIKING_LEGGINS, "viking_leggings");
-        ModItems.register(ModItems.VIKING_BOOTS, "viking_boots");
+        ModItems.registerItem(ModItems.SUSPICIOUS_DUST, "suspicious_dust");
+        ModItems.registerItem(ModItems.THE_MIGHTY_STICK, "the_mighty_stick");
+        ModItems.registerItem(ModItems.ANCIENT_STEEL_SWORD, "ancient_steel_sword");
+        ModItems.registerItem(ModItems.VIKING_HELMET, "viking_helmet");
+        ModItems.registerItem(ModItems.VIKING_VEST, "viking_vest");
+        ModItems.registerItem(ModItems.VIKING_LEGGINS, "viking_leggings");
+        ModItems.registerItem(ModItems.VIKING_BOOTS, "viking_boots");
+        
+        ModItems.registerPotion(ModItems.SUSPICIOUS_POTION, "suspicious_potion");
     }
 
 }
