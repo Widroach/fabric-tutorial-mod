031d923d579d476703b004ad2fa0996d0850268d
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-08-29 16:11:16 +0200

    registering the item to the custom item group

--- src/main/java/com/tutorial/mod/BetterDirt.java
+++ src/main/java/com/tutorial/mod/BetterDirt.java
@@ -1,5 +1,6 @@
 package com.tutorial.mod;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.sound.BlockSoundGroup;
@@ -12,6 +13,12 @@ public class BetterDirt extends Block {
 
     public BetterDirt() {
         super(AbstractBlock.Settings.create().sounds(BlockSoundGroup.GRASS));
+        initialize();
+    }
+
+    private void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ModGroups.TUTORIAL_MOD_ITEM_GROUP_KEY)
+                .register(itemGroup -> itemGroup.add(this.asItem()));
     }
 
 }
