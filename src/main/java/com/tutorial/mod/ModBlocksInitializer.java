474331cba91d28bc568490b6a90013b88c9ea54d
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-08-29 16:11:53 +0200

    initializing the new sample block

--- src/main/java/com/tutorial/mod/ModBlocksInitializer.java
+++ src/main/java/com/tutorial/mod/ModBlocksInitializer.java
@@ -3,10 +3,9 @@ package com.tutorial.mod;
 import net.fabricmc.api.ModInitializer;
 
 public class ModBlocksInitializer implements ModInitializer {
-    private static ModBlocks modBlocks = new ModBlocks();
 
     @Override
     public void onInitialize() {
-        modBlocks.register(modBlocks.getBetterDirt(), "better_dirt", true);
+        ModBlocks.register(ModBlocks.BETTER_DIRT, "better_dirt", true);
     }
 }
