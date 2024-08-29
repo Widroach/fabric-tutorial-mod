a2718ad4b087da143ba2d6c6ca573c669e37c7e9
zalan-hegedus <zalan.hegedus@student.howest.be>
2024-08-29 14:54:37 +0200

    Adding a ModItems register abstraction class

---
+++ src/main/java/com/tutorial/mod/ModBlocks.java
@@ -0,0 +1,23 @@
+package com.tutorial.mod;
+
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    public Block register(Block block, String name, boolean shouldRegister) {
+
+        Identifier id = Identifier.of("tutorial-mod", name);
+
+        if (shouldRegister) {
+            BlockItem blockItem = new BlockItem(block, new Item.Settings());
+            Registry.register(Registries.ITEM, id, blockItem);
+        }
+
+        return Registry.register(Registries.BLOCK, id, block);
+    }
+}
